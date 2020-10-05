e4f131bf3caa2342ae4bbdfa31272ebce17ac19a
npj6 <npj6@alu.ua.es>
2020-10-05 19:18:37 +0200

    entity offsets are now easily modifiable

--- z80-runner/src/manager/entity.h.s
+++ z80-runner/src/manager/entity.h.s
@@ -3,16 +3,16 @@
 .globl entity_enemy
 
 entity_x_speed        = 0
-entity_y_speed        = 1
-entity_x_coord        = 2
-entity_y_coord        = 3
-entity_last_screen    = 4 ;;2
-entity_last_screen_l  = 4
-entity_last_screen_h  = 5
-entity_width          = 6
-entity_height         = 7
-entity_color          = 8
-entity_size           = 9
+entity_y_speed        = entity_x_speed+1
+entity_x_coord        = entity_y_speed+1
+entity_y_coord        = entity_x_coord+1
+entity_last_screen    = entity_y_coord+1 ;;2
+entity_last_screen_l  = entity_last_screen
+entity_last_screen_h  = entity_last_screen+1
+entity_width          = entity_last_screen+2
+entity_height         = entity_width+1
+entity_color          = entity_height+1
+entity_size           = entity_color+1
 
 .macro entity_define
     .rept #entity_size
@@ -20,6 +20,8 @@ entity_size           = 9
     .endm
 .endm
 
+;; As readable and neat this macro might be, it wastes space to some extent
+;; Usual way to do this is having a value-filled prototype in memory, and ldir the _ENTITY you wanna fill
 ;;INPUT:
 ;;  _ENTITY: direct or indirect 2 byte adress
 ;;  _Y_SPEED: 1 byte number or registers: a, b, c, d, e, h, l
@@ -28,7 +30,7 @@ entity_size           = 9
 ;;DESTROYS: ix
 .macro entity_fill _ENTITY, _Y_SPEED, _X_COORD, _Y_COORD, _WIDTH, _HEIGHT, _COLOR
     ld ix, _ENTITY
-    ld entity_x_speed(ix), #0 ;; TODO: temporary
+    ld entity_x_speed(ix), #0 ;; TODO: temporary (everything is, memento mori)
     ld entity_y_speed(ix), _Y_SPEED
     ld entity_x_coord(ix), _X_COORD
     ld entity_y_coord(ix), _Y_COORD
