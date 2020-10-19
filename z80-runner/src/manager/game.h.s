f90d25d0e1b49660dab671185ed6462e02af4490
lccatala <lccm1@alu.ua.es>
2020-10-19 11:39:37 +0200

    Simplified game loop

--- z80-runner/src/manager/game.h.s
+++ z80-runner/src/manager/game.h.s
@@ -3,3 +3,18 @@
 .globl game_level_speed
 .globl game_init
 .globl game_loop
+
+;; Game has ended. Add a bit of delay and write a final message (for either death or victory)
+;;INPUT:
+;;  _MESSAGE: can be **, (**)
+;;DESTROYS: AF, BC, DE, HL, IY
+.macro game_end _MESSAGE
+    ;; Waits a bit so you see what you did!
+    ld a, #25
+    call game_wait_cycles
+
+    call  render_clean
+
+    ;; Final message is written
+    render_draw_text_at #0x08, #0x85, #0, #1, #_MESSAGE
+.endm
--- z80-runner/src/manager/game.s
+++ z80-runner/src/manager/game.s
@@ -36,6 +36,20 @@ game_init::
    call     physics_init
    ret
 
+;; DESTROYS: A
+game_check_end_conditions:
+   ;; If collision was not detected, game continues as usual
+	ld a, (physics_collision_detected)
+
+   ;; Collision with level end
+   cp #0x10
+   jr    z,   game_win
+
+   ;; Collision with enemy
+   cp #0x01
+   jr    z,   game_death
+   ret
+
 ;;DESTROYS: AF, BC, DE, HL, IX, IY
 game_loop::
    call     keyboard_update
@@ -47,54 +61,24 @@ game_loop::
    ld a, #2
    call game_wait_cycles
    
-   ;; If collision was not detected, game continues as usual
-	ld a, (physics_collision_detected)
-
-   ;; Collision with level end
-   cp #0x10
-   jr    z,   game_win
-
-   ;; Collision with enemy
-   cp #0x01
-   jr    z,   game_death
+   call game_check_end_conditions
    jr game_loop
-
 
    ;; Game waits until you press space
    game_death:
+      game_end game_death_message
+      jr game_loop_restart
 
-      ;; Waits a bit so you see how you died!
-      ld a, #25
-      call game_wait_cycles
-
-      ;; If collision was detected screen is cleaned 
-      call  render_clean
-
-      ;; Death message is written
-      render_draw_text_at #0x08, #0x85, #0, #1, #game_death_message
-   game_loop_death:
-      call  keyboard_update
-	   call	keyboard_check_space_just_pressed
-      jr    nz,   game_loop_death
-      jr    game_restart
    game_win:
+      game_end game_win_message
 
-      ;; Waits a bit so you see how you won!
-      ld a, #25
-      call game_wait_cycles
-
-      ;; If collision was detected screen is cleaned 
-      call  render_clean
-
-      ;; Victory message is written
-      render_draw_text_at #0x08, #0x85, #0, #1, #game_win_message
-   game_loop_win:
-      call  keyboard_update
-	   call	keyboard_check_space_just_pressed
-      jr    nz,   game_loop_win
+game_loop_restart:
+   call  keyboard_update
+   call	keyboard_check_space_just_pressed
+   jr    nz,   game_loop_restart
 
 game_restart:
-   ;; After you press space screen cleaned and game is restarted
+   ;; After you press space screen gets cleaned and game is restarted
    call  render_clean
    call  game_init
    jr    game_loop
