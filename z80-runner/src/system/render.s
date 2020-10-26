07f67b3a9aabc1d33539e8669f11eb5ef1897da7
npj6 <npj6@alu.ua.es>
2020-10-26 20:43:56 +0100

    black borders and game name

--- z80-runner/src/system/render.s
+++ z80-runner/src/system/render.s
@@ -104,5 +104,8 @@ render_update::
 	call	render_entity_erase
 	call	render_entity_draw
 
+	render_draw_solid_box_at #0x00, #0x00, #0xC0, #4, #0xC8
+	render_draw_solid_box_at #76, #0x00, #0xC0, #4, #0xC8
+
 	ret
 	
